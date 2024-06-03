d00de93d6c6fe5f1bd0c75e1f6fc9641b9417d8a
iYzee23 <predrag.pesic17@gmail.com>
2024-06-04 00:33:32 +0200

    Implemented logic for SHA-1 hashing.

--- Algorithms/SHA1.py
+++ Algorithms/SHA1.py
@@ -1,9 +1,16 @@
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+
+
 class SHA1:
     @staticmethod
     def generate_hash(data):
-        return
+        if isinstance(data, str):
+            data = data.encode("utf-8")
+        digest = hashes.Hash(hashes.SHA1(), backend=default_backend())
+        digest.update(data)
+        return digest.finalize()
 
     @staticmethod
-    def compare_hashes(hash1,hash2):
+    def compare_hashes(hash1, hash2):
         return hash1 == hash2
-
