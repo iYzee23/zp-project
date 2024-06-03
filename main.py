ee6ec5edd2387217cb54066bf65543b34361304c
nl200370 <nl200370d@student.etf.bg.ac.rs>
2024-06-03 16:39:33 +0200

    Added clases for Algorithms prcessing. FIleUtil finished.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from Algorithms.FileUtil import FileUtil
 print("Hello world")
 print("Hello world")
 print("Hello world")
+f: FileUtil = FileUtil()
+# f.export_PEM("Hello world","PRIVATE_KEY")
+print(f.import_PEM("PRIVATE_KEY"))
