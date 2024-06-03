ae88351eaf41a804436058139badc434f57342ea
iYzee23 <predrag.pesic17@gmail.com>
2024-06-03 16:58:04 +0200

    Refactored FileUtil.py. Tested out the FileUtil in main.py.

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from Structures.PublicRingRow import PublicRingRow
-from Structures.PrivateRingRow import PrivateRingRow
 from Algorithms.FileUtil import FileUtil
 
-print("Hello world")
-print("Hello world")
-print("Hello world")
+"""
+# export txt
+message_txt = "Txt fajl"
+FileUtil.export_message(message_txt)
 
-f: FileUtil = FileUtil()
-f.export_PEM("Hello world","PRIVATE_KEY")
-print(f.import_PEM("PRIVATE_KEY"))
+# import txt
+message_txt = FileUtil.import_message()
+print(message_txt)
+
+# export pem
+message_pem = "Pem fajl"
+FileUtil.export_pem(message_pem, "PRIVATE_KEY")
+
+# import pem
+message_pem = FileUtil.import_pem("PRIVATE_KEY")
+print(message_pem)
+"""
